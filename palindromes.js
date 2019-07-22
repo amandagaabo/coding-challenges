4a4d2d62ce1a8bbccc6e97deecf3b397e8cc3c18
Amanda Herschleb <amanda.herschleb@markit.com>
2019-07-22 09:38:15 -0600

    keep track of longest pal and prevent checking

--- palindromes.js
+++ palindromes.js
@@ -11,18 +11,19 @@ const getPalindrome = s => {
 	if (isPalindrome(s)) return s;
 
 	const length = s.length;
-	const subPalindromes = [];
+	let longestPal = '';
 
 	for (let i = 0; i < length; i++) {
 		for (let j = length; j > 0; j--) {
 			let substring = s.substring(i,j);
-			if (substring.length > 1 && isPalindrome(substring)) subPalindromes.push(substring);
+			if (substring.length > 1 && substring.length > longestPal.length) {
+				const substringIsPalindrome = isPalindrome(substring)
+				longestPal = substringIsPalindrome ? substring : longestPal;
+			};
 		}
 	}
 
-	const highToLow = subPalindromes.sort((a, b) => b.length - a.length);
-	return highToLow[0] || s.charAt(0);
-
+	return longestPal
 }
 
 console.log(getPalindrome('haNnah'));
